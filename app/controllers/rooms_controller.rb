ac0a0a12ce75adbb70f64b30b29b24e571488ca5
nastyxxx <naotsu0418@gmail.com>
2020-11-19 22:47:53 +0900

    message画面の作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.order("created_at DESC")
   end
   
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
   has_one_attached :image
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages, dependent: :destroy
 end
