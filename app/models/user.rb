19346854e3c16333e33be54bcec363729cce257d
nastyxxx <naotsu0418@gmail.com>
2020-11-21 17:30:59 +0900

    新規登録画面、ログイン画面の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,17 @@ class User < ApplicationRecord
   has_many :rooms, through: :room_users, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :sns_credential
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email:    auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
