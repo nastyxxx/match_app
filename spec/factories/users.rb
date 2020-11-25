f3a4e1131abd2a2db5a610d07e448f68e2b062a1
nastyxxx <naotsu0418@gmail.com>
2020-11-25 18:36:30 +0900

    viewの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    introduction {Faker::Lorem.sentence}
+    birth_day {Faker::Date.birthday}
+    address {Faker::Address.city}
   end
 end
