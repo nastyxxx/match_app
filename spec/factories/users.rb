af681df15d04837183582ca6bfd2320ccea90b66
nastyxxx <naotsu0418@gmail.com>
2020-11-27 11:30:24 +0900

    userモデルの単体コード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,9 @@ FactoryBot.define do
     introduction { Faker::Lorem.sentence }
     birth_day { Faker::Date.birthday }
     address { Faker::Address.city }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it "全て存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+
+    it "imageが空では登録できないこと" do
+      @user.image = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "nicknameが空では登録できないこと" do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      @user.password = "aaaaaa"
+      @user.password_confirmation = "aaaaaa"
+      @user.valid?
+      expect(@user).to be_valid
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
+      @user.password = "aaaaa"
+      @user.password_confirmation = "aaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    it "passwordとpassword_confirmationが不一致では登録できないこと" do
+      @user.password = "123456"
+      @user.password_confirmation = "aaaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "birth_dayが空では登録できないこと" do
+      @user.birth_day = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth day can't be blank")
+    end
+
+    it "addressが空では登録できないこと" do
+      @user.address = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Address can't be blank")
+    end
+  end
 end
