17279b932aef2a11312c2f2fe0aa8f8dcf399d9e
nastyxxx <naotsu0418@gmail.com>
2020-12-09 14:11:59 +0900

    unicornの修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../..', __dir__)
+app_path = File.expand_path('../../../', __FILE__)
 
 # アプリケーションサーバの性能を決定する
 worker_processes 1
