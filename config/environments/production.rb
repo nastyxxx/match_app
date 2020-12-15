f6dfff767e66b70c33ecde1c4f63e165bb067b5e
nastyxxx <naotsu0418@gmail.com>
2020-12-15 20:35:48 +0900

    Retouching ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,8 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.assets.js_compressor = :uglifier
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://Elastic IP/cable'
+  config.action_cable.allowed_request_origins = [ 'http://Elastic IP' ]
 end
